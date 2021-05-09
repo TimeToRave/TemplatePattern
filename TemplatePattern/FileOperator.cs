ee94ee29927d3aa0816301270d254d98c92f6e8a
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-05-09 14:17:52 +0300

    Добавление класса для работы с файловой системой

---
+++ TemplatePattern/FileOperator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace TemplatePattern
+
+{
+    /// <summary>
+    /// Класс работы с файловой системой
+    /// </summary>
+    public class FileOperator
+    {
+        /// <summary>
+        /// Читает файл
+        /// </summary>
+        /// <param name="filePath">Адрес файла</param>
+        /// <returns>Результат в стороковом представлении</returns>
+        public string ReadTextFromFile(string filePath)
+        {
+            try
+            {
+                return File.ReadAllText(filePath);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"{filePath} file doesn't exists");
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Записывает строку в файл
+        /// </summary>
+        /// <param name="filePath">Адрес файла</param>
+        /// <param name="content">Записыаемое содержимое
+        /// </param>
+        public void WriteTextToFile(string filePath, string content)
+        {
+            try
+            {
+                File.WriteAllText(filePath, content);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"{filePath} file doesn't exists");
+            }
+        }
+    }
+}
