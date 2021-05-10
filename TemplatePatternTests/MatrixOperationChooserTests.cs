de9e6d3c22f3624e7c559faaa8e5f4a33592219b
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-05-10 13:19:34 +0300

    Реализация тестов для класса определения операции над матрицами

--- TemplatePatternTests/MatrixOperationChooserTests.cs
+++ TemplatePatternTests/MatrixOperationChooserTests.cs
@@ -1,10 +1,47 @@
-﻿using NUnit.Framework;
+﻿using System.Runtime.InteropServices;
+using NUnit.Framework;
+using TemplatePattern;
 
 namespace TemplatePatternTests
 {
     [TestFixture]
     public class MatrixOperationChooserTests
     {
+        [Test]
+        public void MatrixOperationChooser_Transponite()
+        {
+            var chooser = new MatrixOperationChooser();
+            MatrixCalculator calculator = chooser.GetMatrixOperation("transponsite");
+            
+            Assert.IsInstanceOf(typeof(MatrixTranspositeCalculator), calculator);
+        }
+        
+        [Test]
+        public void MatrixOperationChooser_Determinant()
+        {
+            var chooser = new MatrixOperationChooser();
+            MatrixCalculator calculator = chooser.GetMatrixOperation("determinant");
+            
+            Assert.IsInstanceOf(typeof(MatrixDeterminantCalculator), calculator);
+        }
+        
+        [Test]
+        public void MatrixOperationChooser_Summ()
+        {
+            var chooser = new MatrixOperationChooser();
+            MatrixCalculator calculator = chooser.GetMatrixOperation("summ");
+            
+            Assert.IsInstanceOf(typeof(MatrixAdderCalculator), calculator);
+        }
+        
+        [Test]
+        public void MatrixOperationChooser_DefaultOperation()
+        {
+            var chooser = new MatrixOperationChooser();
+            MatrixCalculator calculator = chooser.GetMatrixOperation("NotFound");
+            
+            Assert.IsInstanceOf(typeof(MatrixTranspositeCalculator), calculator);
+        }
         
     }
 }
